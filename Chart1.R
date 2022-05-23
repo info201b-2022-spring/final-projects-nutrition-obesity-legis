6d31d5fb007a8b302fa8a793037fa6a41c92d57d
ttr007 <tyler.t.tr03@gmail.com>
2022-05-22 23:26:54 -0700

    Added color to the bar plot, and added it into the report file

--- Chart1.R
+++ Chart1.R
@@ -1,11 +1,22 @@
 library(tidyverse)
 library(dplyr)
 
-dataset <- read.csv("CDC_Nutrition.csv")
+dataset <- read.csv("Nutrition__Physical_Activity__and_Obesity_-_Behavioral_Risk_Factor_Surveillance_System.csv")
 
 races_overweight <- dataset %>% 
   filter(Question == "Percent of adults aged 18 years and older who have an overweight classification", Race.Ethnicity != "") %>%
   select(Data_Value, Race.Ethnicity) %>%
-  na.omit(Race.Ethnicity)
+  na.omit(Race.Ethnicity) %>%
+  group_by(Race.Ethnicity) %>%
+  summarize(avg_value = mean(Data_Value))
 
-barplot <- ggplot(races_overweight, aes(x=Race.Ethnicity, y=Data_Value)) + geom_bar(stat="identity")
+# http://www.cookbook-r.com/Graphs/Legends_(ggplot2)/ used for help with some of the formatting
+barplot <- ggplot(races_overweight, aes(x=Race.Ethnicity, y=avg_value, fill = Race.Ethnicity)) + geom_bar(stat="identity") +
+  scale_x_discrete(guide = guide_axis(n.dodge=3))  +
+  xlab("Race & Ethnicity") + 
+  ylab("Average % Overweight Classification") +
+  ggtitle("Average % Overweight Classification by Race & Ethnicity") +
+  guides(fill=guide_legend(title="Race & Ethnicity")) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
