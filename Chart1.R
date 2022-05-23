d2722589bfe00c8e2d8b741e0de5607825111ee6
ttr007 <tyler.t.tr03@gmail.com>
2022-05-22 23:31:22 -0700

    added a comment to show what website was used to assist with code

--- Chart1.R
+++ Chart1.R
@@ -11,6 +11,7 @@ races_overweight <- dataset %>%
   summarize(avg_value = mean(Data_Value))
 
 # http://www.cookbook-r.com/Graphs/Legends_(ggplot2)/ used for help with some of the formatting
+# https://stackoverflow.com/questions/35090883/remove-all-of-x-axis-labels-in-ggplot used to figure out how to remove x-axis labels
 barplot <- ggplot(races_overweight, aes(x=Race.Ethnicity, y=avg_value, fill = Race.Ethnicity)) + geom_bar(stat="identity") +
   scale_x_discrete(guide = guide_axis(n.dodge=3))  +
   xlab("Race & Ethnicity") + 
