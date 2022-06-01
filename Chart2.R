5b213f454c3d5da89f275872acedb44acd400cb7
ttr007 <tyler.t.tr03@gmail.com>
2022-05-31 23:12:40 -0700

    changed up chart2

--- Chart2.R
+++ Chart2.R
@@ -3,16 +3,22 @@ library(dplyr)
 
 dataset <- read.csv("Nutrition__Physical_Activity__and_Obesity_-_Behavioral_Risk_Factor_Surveillance_System.csv")
 
-US_counts_by_year <- dataset %>%
-  filter(LocationAbbr == "US") %>%
-  count(ï..YearStart, LocationAbbr)
+US_year_question <- dataset %>%
+  filter(LocationAbbr == "US", Education != "") %>%
+  select(ï..YearStart, Education, Data_Value) %>%
+  na.omit() %>%
+  group_by(Education, ï..YearStart) %>%
+  summarise(avg_value = mean(Data_Value))
 
-
-scatterplot <- ggplot(US_counts_by_year, aes(x=ï..YearStart, y=n)) +
-  geom_point(aes(x=ï..YearStart, y=n)) +
-  geom_line(aes(x=ï..YearStart, y=n)) +
+linechart <- ggplot(US_year_question, aes(ï..YearStart, avg_value)) +
+  geom_line(aes(color = Education)) +
+  labs(title = "Data Values by Education",
+       subtitle = "2020") +
+  theme(
+    plot.title = element_text(size = 14, face = "bold"),
+    plot.subtitle = element_text(size = 12),
+    text = element_text(size = 11)) +
+  xlab("Year") +
+  ylab("Average % Overweight Classification by Education") +
   scale_x_continuous(breaks = seq(2011, 2020)) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  labs(title="Year vs Total Surveys",
-       x="Year",
-       y="Total Surveys")
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
